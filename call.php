417ec3ce76eb44ba439c7af835e71f715bc541cb
manoj <manoj@rudrainnovatives.com>
2015-12-28 12:28:10 +0530

    Update call.php

--- call.php
+++ call.php
@@ -2,3 +2,19 @@ $preset=DeliPreset::model()->with(array('tasks','tasks.preusn','tasks.preusn.usn
 
 
 
+1) Gii
+2) model relationships
+3) Extensions
+4) modules
+5) simple database queries
+6) form validations
+7) themes
+8)Documentations
+9) securities
+10 caching
+11) debugging
+12) sessions
+13) config parameters
+14) speed
+15) Url manager
+http://www.sitepoint.com/7-reasons-choose-yii-2-framework/
